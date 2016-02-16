3cd324ed66fac596e6b92ca1a1085a6c94111240
Ben Allred <skuttleman@gmail.com>
2016-02-16 14:02:11 -0700

    added 'true' and 'false' to affirmative and negative functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   destupidifyAffirmativeVal: function(input) {
     lc = input.toLowerCase();
-    var pos = ['yes', 'yup', 'yep', 'yeah', 'sure', 'i guess', 'i am not opposed to that'];
+    var pos = ['true', 'yes', 'yup', 'yep', 'yeah', 'sure', 'i guess', 'i am not opposed to that'];
     for (var i = 0; i < pos.length; i++) {
       if(lc === pos[i]) {
         return true;
@@ -9,7 +9,7 @@ module.exports = {
     }
   },
   destupidifyNegativeVal: function(input) {
-    var neg = ['no', 'nope', 'nuh-uh', 'nay', 'nah', 'not really', 'no fucking way dude'];
+    var neg = ['false', 'no', 'nope', 'nuh-uh', 'nay', 'nah', 'not really', 'no fucking way dude'];
     lc = input.toLowerCase();
     for (var i = 0; i < neg.length; i++) {
       if(lc === neg[i]) {
